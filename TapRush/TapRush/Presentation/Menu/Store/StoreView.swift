533be3efe661bc113f49012689653eb1b64d80e2
Adam Mitro <adam.mitro@gmail.com>
2025-05-02 14:34:31 -0400

    Add placeholder items for the store

--- TapRush/TapRush/Presentation/Menu/Store/StoreView.swift
+++ TapRush/TapRush/Presentation/Menu/Store/StoreView.swift
@@ -30,12 +30,23 @@ struct StoreView: View {
                             VStack {
                                 // Upgrades
                                 StoreHeaderView(headerName: "Upgrades")
+                                StoreItemView(goldCost: 50, itemName: "Pickaxe")
+                                
+                                StoreItemView(goldCost: 50, itemName: "Inventory")
                                 
                                 // Items
                                 StoreHeaderView(headerName: "Items")
+                                StoreItemView(goldCost: 50, itemName: "Gold Detector")
+                                
+                                StoreItemView(goldCost: 50, itemName: "Magnifying Glass")
+                                StoreItemView(goldCost: 50, itemName: "4 Leaf Clover")
                                 
                                 // Powerups
                                 StoreHeaderView(headerName: "Powerups")
+                                StoreItemView(goldCost: 50, itemName: "Auto Collect")
+                                StoreItemView(goldCost: 50, itemName: "Power Strike")
+                                
+                                StoreItemView(goldCost: 50, itemName: "Blast Off")
                             }
                             .padding(.horizontal, 10)
                         }
@@ -67,7 +78,38 @@ struct StoreHeaderView: View {
                 .frame(height: 2)
                 .frame(maxWidth: .infinity)
         }
-        .padding(.top, 15)
+        .padding(.vertical, 15)
+    }
+}
+
+struct StoreItemView: View {
+    var goldCost: Int
+    var itemName: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "questionmark")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.white)
+                .frame(width: 45, height: 45)
+                .padding()
+                .border(.gray, width: 2)
+            Text(itemName)
+                .frame(width: 150, height: 75, alignment: .leading)
+                .foregroundStyle(.peachOrange)
+            HStack {
+                Text("\(goldCost)x")
+                    .frame(height: 75, alignment: .leading)
+                    .foregroundStyle(.peachOrange)
+                Image("gold")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .padding(.trailing, 10)
+            }
+        }
+        .border(.gray, width: 2)
     }
 }
 
